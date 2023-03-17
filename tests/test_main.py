f1172546f10c168fae7722c4d41e6673890e2920
34j <55338215+34j@users.noreply.github.com>
2023-03-17 12:00:00 +0900

    test: add blank test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
-from so_vits_svc_fork.main import add
+from unittest import TestCase
 
 
-def test_add():
-    assert add(1, 1) == 2
+class TestMain(TestCase):
+    def test_main(self):
+        pass
