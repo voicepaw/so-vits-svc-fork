b1394836132b9ad80d23a8494abc779c421e2a5e
34j <55338215+34j@users.noreply.github.com>
2023-03-25 16:29:50 +0900

    chore(easy): update install.bat

--- easy-installation/install.bat
+++ easy-installation/install.bat
@@ -10,7 +10,7 @@ if %errorlevel%==0 (
     curl https://www.python.org/ftp/python/3.10.10/python-3.10.10-amd64.exe -o python-3.10.10-amd64.exe
 
     echo Installing Python 3.10...
-    python-3.10.10-amd64.exe /quiet InstallAllUsers=0 PrependPath=1
+    python-3.10.10-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
 
     echo Cleaning up installer...
     del python-3.10.10-amd64.exe
@@ -20,25 +20,25 @@ echo Checking GPU...
 nvidia-smi >nul 2>&1
 if %errorlevel%==0 (
     set GPU=1
+) else (
+    set GPU=0
+)
 
+if %GPU%==1 (
     echo Checking CUDA...
     nvcc --version
     if %errorlevel%==0 (
         echo CUDA is already installed.
     ) else (
-        echo Please install CUDA 11.7 from https://developer.nvidia.com/cuda-11-7-0-download-archive?target_os=Windows
-        exit
+        echo Please install CUDA 11.7 from https://developer.nvidia.com/cuda-11-7-0-download-archive?target_os=Windows & exit /b 1
     )
 
     echo Checking cuDNN...
     if exist "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.7\bin\cudnn64_8.dll" (
         echo cuDNN is already installed.
     ) else (
-        echo Please install cuDNN for CUDA 11.x from https://developer.nvidia.com/cudnn (https://developer.nvidia.com/downloads/compute/cudnn/secure/8.8.1/local_installers/11.8/cudnn-windows-x86_64-8.8.1.3_cuda11-archive.zip/)
-        exit
+        echo Please install cuDNN for CUDA 11.x from https://developer.nvidia.com/cudnn (https://developer.nvidia.com/downloads/compute/cudnn/secure/8.8.1/local_installers/11.8/cudnn-windows-x86_64-8.8.1.3_cuda11-archive.zip/) & exit /b 1
     )
-) else (
-    set GPU=0
 )
 
 echo Creating virtual environment...
@@ -49,7 +49,7 @@ venv\Scripts\python.exe -m pip install --upgrade pip wheel
 
 if %GPU%==1 (
     echo Installing PyTorch with GPU support...
-    venv\Scripts\pip.exe install torch torchaudio --index-url https://download.pytorch.org/whl/cu116
+    venv\Scripts\pip.exe install torch torchaudio --index-url https://download.pytorch.org/whl/cu117
 ) else (
     echo Installing PyTorch without GPU support...
     venv\Scripts\pip.exe install torch torchaudio
@@ -60,5 +60,3 @@ venv\Scripts\pip.exe install so-vits-svc-fork
 
 echo Launching so-vits-svc-fork GUI...
 venv\Scripts\svcg.exe
-
-echo Done.
