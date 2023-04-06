58fc0a6dc827dc25d32642859d7f8f025a4d12a4
Exosfeer <exosfeer@protonmail.com>
2023-04-06 19:32:12 +0200

    chore(cuDNN): updated feedback and links.

--- easy-installation/install.bat
+++ easy-installation/install.bat
@@ -30,11 +30,11 @@ if %GPU%==1 (
     if %errorlevel%==0 (
         echo CUDA is already installed.
     ) else (
-        echo Please install CUDA 11.7 from https://developer.nvidia.com/cuda-11-7-0-download-archive?target_os=Windows & exit /b 1
+        echo Please install CUDA 11.7 from https://developer.nvidia.com/cuda-11-8-0-download-archive?target_os=Windows & exit /b 1
     )
 
     echo Checking cuDNN...
-    if exist "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.7\bin\cudnn64_8.dll" (
+    if exist "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\bin\cudnn64_8.dll" (
         echo cuDNN is already installed.
     ) else (
         echo Please install cuDNN for CUDA 11.x from https://developer.nvidia.com/cudnn (https://developer.nvidia.com/downloads/compute/cudnn/secure/8.8.1/local_installers/11.8/cudnn-windows-x86_64-8.8.1.3_cuda11-archive.zip/) & exit /b 1
@@ -49,7 +49,7 @@ venv\Scripts\python.exe -m pip install --upgrade pip wheel
 
 if %GPU%==1 (
     echo Installing PyTorch with GPU support...
-    venv\Scripts\pip.exe install torch torchaudio --index-url https://download.pytorch.org/whl/cu117
+    venv\Scripts\pip.exe install torch torchaudio --index-url https://download.pytorch.org/whl/cu118
 ) else (
     echo Installing PyTorch without GPU support...
     venv\Scripts\pip.exe install torch torchaudio
