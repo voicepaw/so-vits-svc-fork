e83bb85bd9b814e1c381859b104c1538dccfdbd2
34j <55338215+34j@users.noreply.github.com>
2023-04-09 14:55:13 +0900

    fix(commons): fix more

--- src/so_vits_svc_fork/modules/commons.py
+++ src/so_vits_svc_fork/modules/commons.py
@@ -34,10 +34,12 @@ def get_padding(kernel_size, dilation=1):
     return int((kernel_size * dilation - dilation) / 2)
 
 
-def convert_pad_shape(pad_shape):
-    l = pad_shape[::-1]
-    pad_shape = [item for sublist in l for item in sublist]
-    return pad_shape
+def convert_pad_shape(pad_shape) -> torch.Tensor:
+    # l = pad_shape[::-1]
+    # pad_shape = [item for sublist in l for item in sublist]
+    # return pad_shape
+    # more efficient
+    return torch.tensor(pad_shape[::-1]).view(-1)
 
 
 def intersperse(lst, item):
@@ -67,12 +69,7 @@ def rand_gumbel_like(x):
 
 
 def slice_segments(x, ids_str, segment_size=4):
-    ret = torch.zeros_like(x[:, :, :segment_size])
-    for i in range(x.size(0)):
-        idx_str = ids_str[i]
-        idx_end = idx_str + segment_size
-        ret[i] = x[i, :, idx_str:idx_end]
-    return ret
+    return x[:, :, ids_str : ids_str + segment_size]
 
 
 def rand_slice_segments(x, x_lengths=None, segment_size=4):
