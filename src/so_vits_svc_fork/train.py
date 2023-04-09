b260f787eda3b1cfcb217157cff8b02e0adc5105
34j <55338215+34j@users.noreply.github.com>
2023-04-09 22:09:44 +0900

    fix(train): do not save ckpts at iteration 0

--- src/so_vits_svc_fork/train.py
+++ src/so_vits_svc_fork/train.py
@@ -364,6 +364,8 @@ class VitsLightning(pl.LightningModule):
                     "gt/mel": utils.plot_spectrogram_to_numpy(mel[0].cpu().numpy()),
                 }
             )
+            if self.current_epoch == 0:
+                return
             utils.save_checkpoint(
                 self.net_g,
                 self.optim_g,
